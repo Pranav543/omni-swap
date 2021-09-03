ed2d5038828f0e2099ca1f9269c033dd7d4100d9
Pranav Naik <pranavnaik543@gmail.com>
2021-09-03 23:36:26 +0530

    wrote utility functions

--- contracts/pool.py
+++ contracts/pool.py
@@ -20,3 +20,33 @@ class Pool(sp.Contract):
             premiumPool=sp.nat(0),
             isExpired=False,
         )
+
+    @sp.utils.view(sp.timestamp)
+    def getExpirationTimestamp(self):
+        sp.result(self.data.expiryTimestamp)
+
+    @sp.utils.view(sp.TNat)
+    def getCoveragePool(self):
+        sp.result(self.data.coveragePool)
+
+    @sp.utils.view(sp.TNat)
+    def getPremiumPool(self):
+        sp.result(self.data.premiumPool)
+
+    @sp.entry_point
+    def setIsExpiredTrue(self):
+        sp.verify(
+            sp.now > self.data.expiryTimestamp, message="Error: CDS Is Not Expired Yet!"
+        )
+        self.data.isExpired = True
+
+    @sp.entry_point
+    def setIsExpiredTrueForTesting(self):
+        sp.verify(
+            sp.now > self.data.expiryTimestamp, message="Error: CDS Is Not Expired Yet!"
+        )
+        self.data.isExpired = True
+
+    @sp.entry_point
+    def buyCoverage(self, params):
+        self.buyCoverageInternal(sp.sender, params.premiumAmount)
