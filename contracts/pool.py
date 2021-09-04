697f0df664236d38491a32f3eb9469b08f460d59
Pranav Naik <pranavnaik543@gmail.com>
2021-09-04 22:21:03 +0530

    pool contract finished

--- contracts/pool.py
+++ contracts/pool.py
@@ -19,6 +19,7 @@ class Pool(sp.Contract):
             coveragePool=sp.nat(0),
             premiumPool=sp.nat(0),
             isExpired=False,
+            totalPremiumTokenSupply=sp.none,
         )
 
     @sp.utils.view(sp.timestamp)
@@ -41,6 +42,10 @@ class Pool(sp.Contract):
         self.data.isExpired = True
 
     @sp.entry_point
+    def receiveTotalPremiumTokenSupply(self, result):
+        self.data.totalPremiumTokenSupply = sp.some(result)
+
+    @sp.entry_point
     def setIsExpiredTrueForTesting(self):
         sp.verify(
             sp.now > self.data.expiryTimestamp, message="Error: CDS Is Not Expired Yet!"
@@ -51,9 +56,14 @@ class Pool(sp.Contract):
     def calculateCoverTokenAmount(x):
         sp.result(x * 1)
 
-    def buyCoverageInternal(self, buyer, premiumAmount):
+    @sp.sub_entry_point
+    def buyCoverageInternal(self, params):
         sp.transfer(
-            sp.record(from_=buyer, to_=sp.to_address(sp.self), value=premiumAmount),
+            sp.record(
+                from_=params.buyer,
+                to_=sp.to_address(sp.self),
+                value=params.premiumAmount,
+            ),
             sp.tez(0),
             sp.contract(
                 sp.TRecord(from_=sp.TAddress, to_=sp.TAddress, value=sp.TNat),
@@ -61,11 +71,12 @@ class Pool(sp.Contract):
                 "transfer",
             ).open_some(),
         )
-        sp.data.premiumPool += premiumAmount
-        coverTokenAmount = self.calculateCoverTokenAmount(premiumAmount)
+        sp.data.premiumPool += params.premiumAmount
+        coverTokenAmount = sp.local("coverTokenAmount", 0)
+        coverTokenAmount.value = self.calculateCoverTokenAmount(params.premiumAmount)
         sp.transfer(
-            sp.record(address=buyer, value=coverTokenAmount),
-            sp.tez(5),
+            sp.record(address=params.buyer, value=coverTokenAmount.value),
+            sp.tez(0),
             sp.contract(
                 sp.TRecord(address=sp.TAddress, value=sp.TNat),
                 self.data.coverToken,
@@ -75,23 +86,36 @@ class Pool(sp.Contract):
 
     @sp.entry_point
     def buyCoverage(self, params):
-        self.buyCoverageInternal(sp.sender, params.premiumAmount)
+        self.buyCoverageInternal(
+            sp.record(buyer=sp.sender, premiumAmount=params.premiumAmount)
+        )
 
     @sp.global_lambda
-    def calculateCoverTokenValue(self, coverTokenAmount_, totalCoverTokenSupply_):
-        coveragePoolSize_ = self.getCoveragePool()
-        coverTokenValueTemp_ = coveragePoolSize_ * 100 / totalCoverTokenSupply_
-        coverTokenValue_ = coverTokenAmount_ * coverTokenValueTemp_ / 100
-        sp.result(coverTokenValue_)
+    def calculateCoverTokenValue(self, params):
+        coveragePoolSize_ = sp.local("coveragePoolSize_", 0)
+        coverTokenValueTemp_ = sp.local("coverTokenValueTemp_", 0)
+        coverTokenValue_ = sp.local("coverTokenValue_", 0)
+        coveragePoolSize_.value = self.getCoveragePool()
+        coverTokenValueTemp_.value = (
+            coveragePoolSize_.value * 100 / params.totalCoverTokenSupply_
+        )
+        coverTokenValue_.value = (
+            params.coverTokenAmount_ * coverTokenValueTemp_.value / 100
+        )
+        sp.result(coverTokenValue_.value)
 
     @sp.entry_point
     def claimCoverage(self, params):
-        coverageAmount = self.calculateCoverTokenValue(
-            params.coverTokenAmount, params.totalCoverTokenSupply
+        coverageAmount = sp.local("coverageAmount", 0)
+        coverageAmount.value = self.calculateCoverTokenValue(
+            sp.record(
+                coverTokenAmount_=params.coverTokenAmount,
+                totalCoverTokenSupply_=params.totalCoverTokenSupply,
+            )
         )
         sp.transfer(
             sp.record(
-                from_=sp.to_address(sp.self), to_=sp.sender, value=coverageAmount
+                from_=sp.to_address(sp.self), to_=sp.sender, value=coverageAmount.value
             ),
             sp.tez(0),
             sp.contract(
@@ -103,7 +127,7 @@ class Pool(sp.Contract):
 
         sp.transfer(
             sp.record(address=sp.sender, value=params.coverTokenAmount),
-            sp.tez(5),
+            sp.tez(0),
             sp.contract(
                 sp.TRecord(address=sp.TAddress, value=sp.TNat),
                 self.data.coverToken,
@@ -115,9 +139,14 @@ class Pool(sp.Contract):
     def calculatePremiumTokenAmount(x):
         sp.result(x * 1)
 
-    def sellCoverageInternal(self, seller, coverageAmount):
+    @sp.sub_entry_point
+    def sellCoverageInternal(self, params):
         sp.transfer(
-            sp.record(from_=seller, to_=sp.to_address(sp.self), value=coverageAmount),
+            sp.record(
+                from_=params.seller,
+                to_=sp.to_address(sp.self),
+                value=params.coverageAmount,
+            ),
             sp.tez(0),
             sp.contract(
                 sp.TRecord(from_=sp.TAddress, to_=sp.TAddress, value=sp.TNat),
@@ -125,11 +154,14 @@ class Pool(sp.Contract):
                 "transfer",
             ).open_some(),
         )
-        sp.data.coveragePool += coverageAmount
-        premiumTokenAmount = self.calculatePremiumTokenAmount(coverageAmount)
+        sp.data.coveragePool += params.coverageAmount
+        premiumTokenAmount = sp.local("premiumTokenAmount", 0)
+        premiumTokenAmount.value = self.calculatePremiumTokenAmount(
+            params.coverageAmount
+        )
         sp.transfer(
-            sp.record(address=seller, value=premiumTokenAmount),
-            sp.tez(5),
+            sp.record(address=params.seller, value=premiumTokenAmount.value),
+            sp.tez(0),
             sp.contract(
                 sp.TRecord(address=sp.TAddress, value=sp.TNat),
                 self.data.coverToken,
@@ -139,21 +171,45 @@ class Pool(sp.Contract):
 
     @sp.entry_point
     def sellCoverage(self, params):
-        self.sellCoverageInternal(sp.sender, params.coverageAmount)
+        self.sellCoverageInternal(
+            sp.record(seller=sp.sender, coverageAmount=params.coverageAmount)
+        )
 
     @sp.global_lambda
-    def calculatePremiumTokenValue(self, premiumTokenAmount_, totalPremiumTokenSupply_):
-        premiumPoolSize_ = self.getPremiumPool()
-        premiumTokenValueTemp_ = premiumPoolSize_ * 100 / totalPremiumTokenSupply_
-        premiumTokenValue_ = premiumTokenAmount_ * premiumTokenValueTemp_ / 100
-        sp.result(premiumTokenValue_)
+    def calculatePremiumTokenValue(self, premiumTokenAmount_):
+        premiumPoolSize_ = sp.local("premiumPoolSize_", 0)
+        premiumTokenValueTemp_ = sp.local("premiumTokenValueTemp_", 0)
+        premiumTokenValue_ = sp.local("premiumTokenValue_", 0)
+        premiumPoolSize_.value = self.getPremiumPool()
+        premiumTokenValueTemp_.value = (
+            premiumPoolSize_ * 100 / self.data.totalPremiumTokenSupply
+        )
+        premiumTokenValue_.value = (
+            premiumTokenAmount_ * premiumTokenValueTemp_.value / 100
+        )
+        sp.result(premiumTokenValue_.value)
 
     @sp.entry_point
     def withdrawPremium(self, params):
         sp.verify(sp.data.isExpired == True, message="Error: CDS Is Not Expired Yet!")
-        premiumAmount = self.calculatePremiumTokenValue(params.premiumTokenAmount)
+
+        premiumContract = sp.contract(
+            sp.TPair(sp.TUnit, sp.TContract(sp.TNat)),
+            self.data.premiumToken,
+            entry_point="getTotalSupply",
+        ).open_some("Invalid contract")
         sp.transfer(
-            sp.record(from_=sp.to_address(sp.self), to_=sp.sender, value=premiumAmount),
+            (sp.unit, sp.self_entry_point("receiveTotalPremiumTokenSupply")),
+            sp.mutez(0),
+            premiumContract,
+        )
+
+        premiumAmount = sp.local("premiumAmount", 0)
+        premiumAmount.value = self.calculatePremiumTokenValue(params.premiumTokenAmount)
+        sp.transfer(
+            sp.record(
+                from_=sp.to_address(sp.self), to_=sp.sender, value=premiumAmount.value
+            ),
             sp.tez(0),
             sp.contract(
                 sp.TRecord(from_=sp.TAddress, to_=sp.TAddress, value=sp.TNat),
@@ -164,7 +220,7 @@ class Pool(sp.Contract):
         sp.data.premiumPool -= premiumAmount
         sp.transfer(
             sp.record(address=sp.sender, value=params.premiumTokenAmount),
-            sp.tez(5),
+            sp.tez(0),
             sp.contract(
                 sp.TRecord(address=sp.TAddress, value=sp.TNat),
                 self.data.premiumToken,
